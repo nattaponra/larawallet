63aae430408a2fdf7ea20389fb3ab35c03fa8243
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-06-16 14:10:45 +0700

    Add Facade

---
+++ src/LaraWalletFacade.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace nattaponra\LaraWallet;
+use Illuminate\Support\Facades\Facade;
+/**
+ * @see \Nattaponra\LaraWallet\WalletFacade
+ */
+class WalletFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'LaraWallet';
+    }
+}
