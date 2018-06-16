3d659919814a19c70306caa82e25d1f8f3bd89aa
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-06-16 13:41:39 +0700

    Create trait

---
+++ src/HasWallet.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nattaponra
+ * Date: 16/6/2561
+ * Time: 13:26 น.
+ */
+
+namespace nattaponra\LaraWallet;
+
+
+trait HasWallet
+{
+
+    public function balance(){
+        return 100;
+    }
+
+    public function deposit(){
+
+    }
+
+    public function withdraw(){
+
+    }
+}
