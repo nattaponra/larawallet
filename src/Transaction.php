165c8e88ca4c79717b2aee988242eeb090f48b3f
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-06-17 12:07:24 +0700

    Add transaction model

---
+++ src/Transaction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace nattaponra\LaraWallet;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $fillable = ["wallet_id","transaction_type","amount","deposit"];
+
+    public function __construct(array $attributes = [])
+    {
+        $this->table = config("larawallet.transaction_table","lara_wallet_transactions");
+        parent::__construct($attributes);
+    }
+
+   
+
+}
+
