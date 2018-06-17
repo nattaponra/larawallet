a786f7d00f446bc8753c6fc657960a8a7fbfa1b7
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-06-17 12:06:00 +0700

    Create method to build transaction

--- src/Wallet.php
+++ src/Wallet.php
@@ -1,15 +1,86 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nattaponra
- * Date: 16/6/2561
- * Time: 13:40 น.
- */
 
 namespace nattaponra\LaraWallet;
 
+use Illuminate\Database\Eloquent\Model;
 
-class Wallet
+class Wallet extends Model
 {
 
+
+    protected $fillable = ["user_id","balance"];
+
+    public function __construct(array $attributes = [])
+    {
+        $this->table = config("larawallet.wallet_table","lara_wallet_wallets");
+        parent::__construct($attributes);
+    }
+
+
+    public function balance(){
+        return $this->balance;
+    }
+
+    public function deposit($amount){
+
+        $this->balance += $amount;
+        $this->save();
+
+        $this->transactions()->create([
+            'wallet_id'        => $this->id,
+            'transaction_type' => 'deposit',
+            'amount'           => $amount
+        ]);
+    }
+
+    public function received($amount){
+        $this->balance += $amount;
+        $this->save();
+
+        $this->transactions()->create([
+            'wallet_id'        => $this->id,
+            'transaction_type' => 'received',
+            'amount'           => $amount
+        ]);
+    }
+
+    public function withdraw($amount){
+
+        $this->balance -= $amount;
+        $this->save();
+
+        $this->transactions()->create([
+            'wallet_id'        => $this->id,
+            'transaction_type' => 'withdraw',
+            'amount'           => $amount
+        ]);
+    }
+
+    public function transfer($amount , $toUser){
+
+        $this->balance -= $amount;
+        $this->save();
+
+        $this->transactions()->create([
+            'wallet_id'        => $this->id,
+            'transaction_type' => 'transfer',
+            'amount'           => $amount
+        ]);
+
+        $toUser->wallet->received($amount);
+
+    }
+
+
+
+    public function transactions()
+    {
+        return $this->hasMany( Transaction::class);
+    }
+
+
+    public function user()
+    {
+        return $this->belongsTo(\App\User::class);
+    }
 }
